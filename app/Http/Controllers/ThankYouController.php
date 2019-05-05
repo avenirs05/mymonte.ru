79f37fe421a06c5540dc1993bcc9120e5613c617
avenirs05 <avenirs05@gmail.com>
2019-05-05 11:35:16 +0000

    last

--- app/Http/Controllers/ThankYouController.php
+++ app/Http/Controllers/ThankYouController.php
@@ -11,33 +11,32 @@ class ThankYouController extends Controller
     public function __invoke(Request $request) {
 		$title = Lang::get('text.title_tag.thank_you');
 		$phone_main = Parameter::where('param', 'phone_main')->get()->toArray()[0]['value'];
-//		dd($request->toArray());
 		
 		$subject = "Новое сообщение с сайта mymonte.ru";
 		$headers = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
+		$name = isset($request->name) ? $request->name : 'Не заполнено';
+		$phone = isset($request->phone) ? $request->phone : 'Не заполнено';
+		$email = isset($request->email) ? $request->email : 'Не заполнено';
+		$budget = isset($request->budget) ? $request->budget : 'Не заполнено';		
 		
+		$message = '
+			<html>
+				<head>
+					<title>Новое сообщение с сайта</title>
+				</head>
+				<body style="font-family:Verdana,sans-serif;">
+					<p><b>Имя:</b> "' . $name . '"</p>
+					<p><b>Телефон:</b> "' . $phone . '"</p>
+					<p><b>Email</b> "' . $email . '"</p>
+					<p><b>Бюджет</b> "' . $budget . '"</p>
+				</body>
+			</html>
+		';
 		
-//		$message = '
-//			<html>
-//				<head>
-//					<title>Новое сообщение с сайта</title>
-//				</head>
-//				<body style="font-family:Verdana,sans-serif;">
-//					<p><b>Имя:</b> "' . $name . '"</p>
-//					<p><b>Телефон:</b> "' . $phone . '"</p>
-//					<p><b>Email</b> "' . $email . '"</p>
-//					<p><b>Сообщение:</b><br>"' . $message . '</p><br>----------<br>
-//				</body>
-//			</html>
-//		';
-		
-		mail("domosed365365@gmail.com", $subject, 'Hello Allen, I am about job', $headers);
-		
+		mail("domosed365365@gmail.com", $subject, $message, $headers);		
 	
-	
-
 		$data = [
 			'title' => $title,
 			'phone_main' => $phone_main,
